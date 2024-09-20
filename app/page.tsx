313e1dbb7c10f3f19ed605d5f8e3a4ba3e46b784
Mitansh Chaudhari <mitansh807@gmail.com>
2024-09-19 22:58:34 -0500

    Updated home page message again

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="container">
-      <h1>Hi, I'm Mitansh</h1>
+      <h1>Hi, I'm Mitansh!!</h1>
       <p>Welcome to my simple web application deployed using Vercel!!</p>
     </div>
   );
